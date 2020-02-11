ca347d9e020ed4475ba410e9229ff913cf13cceb
rudovik <flashwrite@ukr.net>
2020-02-11 13:17:56 +0200

    Updates

---
+++ precache-manifest.71b6aaa1d909bb7f3b3b213d8eb38a16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e004be3b0e3b317c96650ddaf1fd72b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c0e1e3e28c56eae7c70c",
+    "url": "/monsters-rolodex/static/css/main.19e37d44.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c0e1e3e28c56eae7c70c",
+    "url": "/monsters-rolodex/static/js/main.e451d8f7.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
